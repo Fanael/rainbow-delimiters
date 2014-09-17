3d425a85d39c589b7e301050e4303a0417a21d7d
Sebastian Wiesner <swiesner@lunaryorn.com>
2014-09-17 11:09:24 +0200

    Don't enable in Completion List Mode

--- rainbow-delimiters.el
+++ rainbow-delimiters.el
@@ -108,7 +108,7 @@
   :group 'applications)
 
 (defcustom rainbow-delimiters-ignore-modes
-  '(special-mode eshell-mode)
+  '(special-mode eshell-mode completion-list-mode)
   "List of modes in which `rainbow-delimiters-mode' should not be automatically enabled.
 
 If the mode of a buffer is derived from one of these modes, then
