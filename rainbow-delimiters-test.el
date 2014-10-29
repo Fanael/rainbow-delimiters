18b9410b6b1ac3857e36d3d329a68d56fb228bd0
Fanael Linithien <fanael4@gmail.com>
2014-10-29 21:26:36 +0100

    Add a test checking if syntax table entries as properties work.

--- rainbow-delimiters-test.el
+++ rainbow-delimiters-test.el
@@ -264,5 +264,18 @@
                    1 2 (face (rainbow-delimiters-depth-1-face))
                    2 3 (face (rainbow-delimiters-depth-1-face)))))))))
 
+(ert-deftest syntax-table-entries-as-properties-work ()
+  (with-temp-buffer-in-mode 'emacs-lisp-mode
+    (with-string (str #("(ab)"
+                        1 2 (syntax-table (4 . ?b))
+                        2 3 (syntax-table (5 . ?a)))))
+    (should (ert-equal-including-properties
+             (buffer-string-without-uninteresting-properties)
+             #("(ab)"
+               0 1 (face (rainbow-delimiters-depth-1-face))
+               1 2 (face (rainbow-delimiters-depth-2-face))
+               2 3 (face (rainbow-delimiters-depth-2-face))
+               3 4 (face (rainbow-delimiters-depth-1-face)))))))
+
 (provide 'rainbow-delimiters-test)
 ;;; rainbow-delimiters-test.el ends here
