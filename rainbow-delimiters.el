27d21f278b748dfa7de2657f8d61ab827f24b6d2
Fanael Linithien <fanael4@gmail.com>
2014-12-18 19:54:43 +0100

    Rephrase the header line description.

--- rainbow-delimiters.el
+++ rainbow-delimiters.el
@@ -1,4 +1,4 @@
-;;; rainbow-delimiters.el --- Highlight nested brackets a different color at each depth -*- lexical-binding: t -*-
+;;; rainbow-delimiters.el --- Highlight brackets according to their each depth -*- lexical-binding: t -*-
 
 ;; Copyright (C)
 ;;   2010-2013 Jeremy Rayman
