9eb2dff24efd977aed184f9fa33516f480cb843f
Fanael Linithien <fanael4@gmail.com>
2014-12-21 18:25:55 +0100

    Release version 2.1.

--- rainbow-delimiters.el
+++ rainbow-delimiters.el
@@ -7,7 +7,7 @@
 ;;         Fanael Linithien <fanael4@gmail.com>
 ;; Maintainer: Fanael Linithien <fanael4@gmail.com>
 ;; Created: 2010-09-02
-;; Version: 2.0.1
+;; Version: 2.1
 ;; Keywords: faces, convenience, lisp, tools
 ;; Homepage: https://github.com/Fanael/rainbow-delimiters
 
